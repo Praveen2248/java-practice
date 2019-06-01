926e0f00c0da07d4b6f9f2e2f9cafe38c19c572e
Praveen2248 <java.praveen95@gmail.com>
2019-06-01 11:52:30 +0530

    I have writeen some comments in orderserve.java

--- src/com/ch/resource/OrderService.java
+++ src/com/ch/resource/OrderService.java
@@ -26,6 +26,7 @@ public class OrderService {
 		int c = 0;
 		while((c=in.read()) != -1) {
 			buffer.append((char)c);
+			//Hi this is praveen kumar working with git hub
 		}
 		return buffer.toString() + " 123 ";
 	}
